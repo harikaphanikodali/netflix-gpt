e32baa2ef033d481400f98f84c28107c58069545
Harika <harikaphanikodali@gmail.com>
2024-03-20 15:22:44 -0400

    form validation, fire base setup, sigin up&singnin user using firebase

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,10 +1,56 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "../utils/firebase"
 
 const Login = () => {
     const [isSignInForm, setSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage]= useState(null)
+    const email = useRef(null);
+    const password = useRef(null);
+
     const toggleSignInForm = () => {
         setSignInForm(!isSignInForm)
+    }
+
+    const  handleButtonClick = () =>{
+       const message =  checkValidData(email.current.value, password.current.value)
+
+       setErrorMessage(message);
+
+       if(message) return;
+
+       if(!isSignInForm){
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          console.log(user);
+     
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+         setErrorMessage(errorCode+ "-" + errorMessage)
+        });
+
+       }else{
+
+             signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                setErrorMessage(errorCode+ "-"+errorMessage)
+            });
+
+       }
+
 
     }
 
@@ -15,17 +61,17 @@ const Login = () => {
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/1c01ae52-0a90-41c5-a3ab-2736447a92ed/US-en-20240311-popsignuptwoweeks-perspective_alpha_website_small.jpg"
                     alt="logo" />
             </div>
-            <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-90">
+            <form  onSubmit={(e)=> e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-90">
                 <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In" : " Sign Up"}</h1>
                 {!isSignInForm && (<input type="text" placeholder="Full Name"
                     className="p-4 my-4 w-full bg-gray-700" />)}
-                <input type="text" placeholder="Email Address"
+                <input type="text" ref={email} placeholder="Email Address"
                     className="p-4 my-4 w-full bg-gray-700" />
-                <input type="password" placeholder="Password"
+                <input type="password" ref={password} placeholder="Password"
                     className="p-4 my-4 w-full bg-gray-700" />
 
-
-                <button className="p-4 my-6 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In" : " Sign Up"}</button>
+                <p className="text-red-500 font-bold text-lg">{errorMessage}</p>
+                <button className="p-4 my-6 bg-red-700 w-full rounded-lg"onClick={handleButtonClick} >{isSignInForm ? "Sign In" : " Sign Up"}</button>
                 <p className="py-4 cursor-pointer " onClick={toggleSignInForm} > {isSignInForm ? "New to netflix? Sign Up Now" : " Already Registered? Sign In Now"}</p>
             </form>
         </div>);
---
+++ netflix-gpt/src/utils/validate.js
@@ -0,0 +1,11 @@
+export const checkValidData = (email, password) =>{
+
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isPaswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
+
+   if (!isEmailValid) return "Email ID not valid"
+   if(!isPaswordValid) return "Password is not valid"
+
+   return null;
+
+}
