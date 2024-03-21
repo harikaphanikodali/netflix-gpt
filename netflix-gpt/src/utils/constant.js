d369eee4b968afe939a7f395bc8c0b5c16a7ca41
Harika <harikaphanikodali@gmail.com>
2024-03-21 15:24:56 -0400

    creating custom hook to fetch nowplayingmovies from tmdb and updating store with movies by creating movie sclice

--- netflix-gpt/src/utils/constant.js
+++ netflix-gpt/src/utils/constant.js
@@ -1,3 +1,12 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const USER_AVATAR = "https://occ-0-2430-2433.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXU-adNpqnNSjBHqT4dxPfIy3IkrPK0JUcjse5tC6Coy2Sdu7SupSAiTbxjzc3veVyPVrYiiqL6wVdM7USBTlWbDY7dEQMpGLtw0h68OMCkGLlkKpFxwIL3ddm9Nzow05-BWJ4_CkuW7dw.png?r=e6e"
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjkwYjQ0NmRkZWEzMWU4ZWE0NGUxYTYzNWVjY2E1MiIsInN1YiI6IjY1ZmM3ZTljODExNDBmMDE3ZTBmY2EzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c6lloP6_JM8W5MPmI1mLYz6EbNGF-bqktF7xARd2zp8'
+    }
+  };
+  
